ab69f9128f76e620c97fdfc05423fd1a2b7621a6
Leeyoung888 <72803116+Leeyoung888@users.noreply.github.com>
2020-10-13 21:29:55 +0800

    在线更新

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -6,11 +6,11 @@ public class HelloGit {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello Git!");
-		System.out.println("更新1!");
-		System.out.println("创建分支!");
-		System.out.println("主干添加!");
-		System.out.println("分支添加!");
-		
+		System.out.println("鏇存柊1!");
+		System.out.println("鍒涘缓鍒嗘敮!");
+		System.out.println("涓诲共娣诲姞!");
+		System.out.println("鍒嗘敮娣诲姞!");
+		System.out.println("鍦ㄧ嚎娣诲姞!");
 
 	}
 
