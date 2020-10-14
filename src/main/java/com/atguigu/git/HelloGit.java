b288c2815464094c0e68cff5f7373140df39901a
Leeyoung888 <liyong1991com@163.com>
2020-10-14 14:38:47 +0800

    eclipse修改后添加

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -4,19 +4,13 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("Hello Git!");
 		System.out.println("更新1!");
 		System.out.println("创建分支!");
 		System.out.println("主干添加!");
 		System.out.println("分支添加!");
-<<<<<<< HEAD
-		System.out.println("在线添加!");
 		System.out.println("修改后添加!");
-
-=======
-		System.out.println("在线添加1!");
->>>>>>> branch 'master' of https://github.com/Leeyoung888/testGit.git
 
 	}
 
