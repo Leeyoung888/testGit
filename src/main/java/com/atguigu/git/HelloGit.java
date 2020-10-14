9eb8b4d338f96e8b9aaaa609eebb0ac38ed52b33
Leeyoung888 <72803116+Leeyoung888@users.noreply.github.com>
2020-10-14 14:37:16 +0800

    在线更新

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -10,13 +10,10 @@ public class HelloGit {
 		System.out.println("创建分支!");
 		System.out.println("主干添加!");
 		System.out.println("分支添加!");
-<<<<<<< HEAD
-		System.out.println("在线添加!");
-		System.out.println("修改后添加!");
+		System.out.println("在线更新!");
 
-=======
-		System.out.println("在线添加1!");
->>>>>>> branch 'master' of https://github.com/Leeyoung888/testGit.git
+
+
 
 	}
 
